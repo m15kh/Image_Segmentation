07d0de67a3d336c5978e0999cbcb215d101d9346
m15kh <mohammad.khalili1515@gmail.com>
2025-04-26 06:23:51 +0000

    edit bash

--- DeepLabV3Plus_Pytorch/bash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Clone the DeepLabV3Plus-Pytorch repository
-git clone https://github.com/VainF/DeepLabV3Plus-Pytorch.git
---
+++ DeepLabV3Plus_Pytorch/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Clone the DeepLabV3Plus-Pytorch repository into a folder named DeeplabV3
+git clone https://github.com/VainF/DeepLabV3Plus-Pytorch.git DeeplabV3
